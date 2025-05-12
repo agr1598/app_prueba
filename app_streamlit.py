c3a42cab1a5ccb1942f116d307c9dc998d6adb22
agr1598 <70321952+agr1598@users.noreply.github.com>
2025-05-12 10:43:21 +0200

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -2,8 +2,19 @@
 import streamlit as st
 import pandas as pd
 
-# Simulación del DataFrame con los datos proporcionados
+# Estilo para fondo blanco
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: white;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
+# Cargar el archivo Excel
 df = pd.read_excel("df.xlsx")
 
 # Título de la app
@@ -14,9 +25,10 @@ search_input = st.text_input("Ingrese el QName a buscar:")
 
 # Si se ingresa algún texto, se filtran los datos
 if search_input:
-    # Se filtra el DataFrame usando una búsqueda que no distingue mayúsculas/minúsculas
+    # Filtrar DataFrame sin distinguir mayúsculas/minúsculas
     filtered_df = df[df["QName"].str.contains(search_input, case=False, na=False)]
     st.write("Resultados de la búsqueda:")
     st.dataframe(filtered_df)
 else:
     st.write("Ingrese un valor en el campo de búsqueda para ver resultados.")
+
